a64d295979679df9c3ac6b8a81c63a6f5c52ec69
yoshi <studyts744@gmail.com>
2019-02-19 04:44:02 +0000

    task status

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -17,8 +17,8 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
-                    {!! Form::label('content', 'ステータス:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -17,8 +17,8 @@
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
                 <div class="form-group">
-                    {!! Form::label('content', 'ステータス:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
