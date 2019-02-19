083a4db34aab107a701a2594f5623dce927af3e4
yoshi <studyts744@gmail.com>
2019-02-19 05:03:48 +0000

    kadai-tasklist

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -26,7 +26,7 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
         
-                {!! Form::submit('更新', ['class' => 'btn btn-light']) !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-dark']) !!}
         
             {!! Form::close() !!}
         </div>
